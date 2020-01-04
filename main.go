0c6bf3e4ea44cfd042ee650f8bbd5683757a097d
Surush <davlatzoda.surush@gmail.com>
2020-01-04 11:07:11 +0500

    initial version

---
+++ main.go
@@ -0,0 +1,15 @@
+package main // ctrl  + Backspace - delete слово
+// любой файл в Go должен лежать в пакете
+// main - это то, что можно запускать
+//import библиотека
+import "fmt"
+
+// Shift + Enter - умный перевод строки
+//main + Tag - live templates, дописывает за тебя
+// Convention over Configuration -> Соглашения вместо конфигурирования
+func main() {
+	// Ctrl + Shift + F10 - запуск компиляции
+	fmt.Println("Hello Go")
+}
+// Alt + F12 Terminal
+//shift + esk = -terminal
